b747505221668b03c7d37fb1b584afb4283fa3bd
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-10-07 21:48:10 +0200

    better inform upon utils and methods

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,28 +1,49 @@
-import { PackageJSON } from './package';
+import { PackageJSON, Exports } from './package';
 import { TSConfig } from './tsconfig';
 import { Plugin } from 'rollup';
+import { DotenvParseOutput } from 'dotenv';
 
-export function jsonmin(content: string): string;
+type EnvVars = 'prod' | 'dev' | 'watch';
 
-type IEnvVars = 'prod' | 'dev' | 'watch'
-
-type IEnv = (
-  condition: IEnvVars
+type Env = (
+  /**
+   * The `--envronment` flag value
+   */
+  condition: EnvVars
 ) => (
   initial: string | string[] | Plugin[] | boolean
-) => (
-  combined: string | string[] | Plugin[] | boolean
-) => []
+) => (combined: string | string[] | Plugin[] | boolean) => [];
+
+/**
+ * Resolves a path to the current working directory.
+ *
+ * @example
+ *
+ * path('file.js') // => 'User/dir/project/file.js'
+ */
+export declare const path: (uri: string) => string;
+
+/**
+ * Minify JSON and/or JSONC files.
+ */
+export declare const jsonmin: (content: string) => string;
+
+/**
+ * Banner - Applies a licence banner to output bundles generated
+ * by rollup for proprietary software of the project
+ */
+export declare const banner: (
+  license: 'MIT' | 'PROPRIETARY' | 'CC BY-NC-ND 4.0'
+) => string;
 
 /**
  * Validates env variable and executes condition based on result.
  */
-export const env: {
-
+export declare const env: {
   /**
    * Returns the parsed contents of a `.env` file
    */
-  get vars(): boolean;
+  get vars(): DotenvParseOutput;
   /**
    * Sugar getter for `process.env.prod` flag, returning `true`
    * when in prod mode else `false`
@@ -42,15 +63,24 @@ export const env: {
    * Tests a predefined condition environment variable. When true,
    * the `returns` value is returned, else `false`
    */
-  is?:(
-    condition: IEnvVars,
+  is?: (
+    condition: EnvVars,
     returns: boolean | string | Plugin | string[]
-  ) => boolean | string | Plugin | string[]
+  ) => boolean | string | Plugin | string[];
   /**
    * Tests a condition for execution. It will concatenates returning array types.
    * by spreading. when condition equates to `false` combined is returned.
    *
-   * ---
+   *
+   * Plugins Example:
+   *
+   * ```js
+   * // dev === true  > Returns initial array
+   * env.if('dev')([plugin1()])([ plugin2() ]) => [ plugin1() ]
+   *
+   * // dev === false > Returns combined array
+   * env.if('dev')([ plugin1() ])([ plugin2() ]) => [ plugin1(), plugin2() ]
+   * ```
    *
    * String to Array Example:
    *
@@ -64,27 +94,17 @@ export const env: {
    * // dev === false > Returns combined array
    * env.if('dev')('foo')('bar') => ['foo', 'bar']
    * ```
-   * ---
-   *
-   * Plugins Example:
-   *
-   * ```js
-   * // dev === true  > Returns initial array
-   * env.if('dev')([plugin1()])([ plugin2() ]) => [ plugin1() ]
-   *
-   * // dev === false > Returns combined array
-   * env.if('dev')([ plugin1() ])([ plugin2() ]) => [ plugin1(), plugin2() ]
-   * ```
    */
-  if?: IEnv;
+  if?: Env;
 };
 
 /**
  * Returns configuration of the current directory and package
  */
-export const config: {
+export declare const config: {
   /**
-   * Returns Current Working Directory path
+   * Returns current working directory path determined
+   * by the value of `process.cwd()`
    */
   get cwd(): string;
   /**
@@ -99,11 +119,11 @@ export const config: {
    * Returns `package.json` dependencies as keys
    */
   get external(): string[];
-   /**
+  /**
    * Returns the Current Working Directory relative
    * resolved path
    */
-  path: (path: string) => string
+  path: (path: string) => string;
   /**
    * Return Alias mapping of paths, used in-conjunction
    * with `@rollup/plugin-alias` and passed to the `entries` field.
@@ -111,24 +131,39 @@ export const config: {
    * @param {string[]} ids List of paths to alias
    * @param {string} [src] Directory to start mapping from
    */
-  alias:(ids: string[], src: 'src' | string) => Array<{
-    find: string,
-    replacement: string
+  alias: (
+    ids: string[],
+    src: 'src' | string
+  ) => Array<{
+    find: string;
+    replacement: string;
   }>;
 
   output: {
-    get cjs (): string | null;
-    get esm (): string | null;
-    get exports(): string | object | null;
-    get main (): string | null;
-    get module (): string | null;
-  }
+    /**
+     * Returns the value of `exports.require` in the
+     * projects `package.json` file.
+     */
+    get cjs(): string | null;
+    /**
+     * Returns the value of `exports.import` in the
+     * projects `package.json` file.
+     */
+    get esm(): string | null;
+    /**
+     * Returns the `exports` value in the
+     * projects `package.json` file.
+     */
+    get exports(): string | Exports | null;
+    /**
+     * Returns the value of the `main` field in the
+     * projects `package.json` file.
+     */
+    get main(): string | null;
+    /**
+     * Returns the value of the `module` field in the
+     * projects `package.json` file.
+     */
+    get module(): string | null;
+  };
 };
-
-/**
- * Banner - Applies a licence banner to output bundles generated
- * by rollup for proprietary software of the project
- */
-export function banner(
-  license: 'MIT' | 'PROPRIETARY' | 'CC BY-NC-ND 4.0'
-): string;
